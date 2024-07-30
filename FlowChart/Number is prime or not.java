53462f1e82a90dbef688a3f52f5d37c5166205b8
amit2041 <av8908042@gmail.com>
2024-07-30 14:04:20 +0530

    Third Commit

---
+++ FlowChart/Number is prime or not.java
@@ -0,0 +1,47 @@
+ public static boolean isPrime(int n) {
+
+        if (n < 2) {
+
+            return false;
+
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (n % i == 0) {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print("Enter a positive integer: ");
+
+        int N = scanner.nextInt();
+
+
+
+        if (isPrime(N)) {
+
+            System.out.println(N + " is a prime number.");
+
+        } else {
+
+            System.out.println(N + " is not a prime number.");
+
+        }
+
+    }
